ce2308adc01f7b73b9e27b814fb437cfdbbcdc5c
ilhamhadisaputro <ilhamhadisaputro28@gmail.com>
2024-03-25 02:50:00 +0700

    awal

---
+++ main.js
@@ -0,0 +1,27 @@
+// // alert('heyy ho!!')
+
+// let nama = "Ilham hadi"; //tipe string
+// let usia = 20; //tipe integer number
+// let tinggiBadan = 170.5; // tipe double floadt
+// let pacar = 1;
+
+// // if (pacar == null) {
+// //     pacar = "belum punya";
+// // } else {
+// //     pacar = "udah punya";
+// // }
+
+// switch(pacar) {
+//     case 1:
+//         pacar = "punya 1 saja Aprillia";
+//     break;
+//     case 2:
+//         pacar = "punya pacar 2, aku flayboy";
+//     break;
+//     default:
+//         pacar = "belum punya pacar";
+// }
+
+// alert(
+//     `nama saya ${nama} usia saya ${usia} 
+//     tinggi badan saya ${tinggiBadan} pacar saya ${pacar}`);
